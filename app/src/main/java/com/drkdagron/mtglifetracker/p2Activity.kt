1155823c6fc06476ca6dda8f20509082d3460766
Stephen McKillop <smckillop@innovada.ca>
2018-11-02 15:52:21 -0400

    initial commit

---
+++ app/src/main/java/com/drkdagron/mtglifetracker/p2Activity.kt
@@ -0,0 +1,12 @@
+package com.drkdagron.mtglifetracker
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class p2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_p2)
+    }
+}
